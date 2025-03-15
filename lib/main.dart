708f4980efdc3687ba67791b76ec2b67df76a1ff
UMIDJON <umidjon@gmail.com>
2025-03-15 23:57:33 +0500

    first algorithm if

---
+++ lib/main.dart
@@ -0,0 +1,336 @@
+
+void main(){
+  //task1
+  // int a = 8;
+  // double plus = 0;
+  // double kopaytiruv = 0;
+  // if(a > 10){
+  //   plus = a + 3;
+  //   print(plus);
+  // }else {
+  //   kopaytiruv = a * 2;
+  //   print(kopaytiruv);
+  // }
+
+  // task2
+  // int a = 9;
+  // int result = 0;
+  // result = a ~/ 3;
+  // print(result);
+
+  // task3
+  // int a = 8;
+  // int result = 0;
+  // result = a % 3;
+  // print(result);
+
+  // task4
+  // int a = 99;
+  // if(a.isOdd){
+  //   print("$a soni toq son");
+  // }else if(a.isEven){
+  //   print("$a soni juft son");
+  // }
+
+  // task5
+  // int a = 12;
+  // int b = 1;
+  // if(a > b){
+  //   print("$a soni katta $b sondan");
+  // }else if(b > a){
+  //   print("$b soni katta $a sondan");
+  // }else if (a == b){
+  //   print("Ular o'zaro teng sonlar");
+  // }
+
+  // task6
+  // int a = 190002;
+  // int b = 999;
+  // int c = 90900;
+  // if(a > b && a > c){
+  //   print("katta son: $a");
+  // }else if(b > a && b > c){
+  //   print("katta son: $b");
+  // }else if (c > a && c > b){
+  //   print("katta son: $c");
+  // }
+
+  // task 7
+  // int a = -1;
+  // if(a > 0){
+  //   print('$a musbat');
+  // }else if (a < 0){
+  //   print("$a manfiy");
+  // }else{
+  //   print("$a nol");
+  // }
+
+  // task 8
+  // int a = 5;
+  // if(a % 5 == 0){
+  //   print("true");
+  // }else{
+  //   print("false");
+  // }
+
+  // task 9
+  // int a = 14;
+  // if(a % 3== 0 && a % 4 == 0){
+  //   print("true");
+  // }else{
+  //   print("false");
+  // }
+
+  // task 10
+ //  int kabisa = 2000;
+ // if(kabisa % 4 == 0 && kabisa % 400 == 0 && kabisa % 100 != 0){
+ //   print("true");
+ // }else{
+ //   print("false");
+ // }
+
+  // task 11
+  // dynamic a = "12";
+  // if(a is int){
+  //   print('son');
+  // }else{
+  //   print('alpha');
+  // }
+
+  // task 12
+  // String a = 'Aa';
+  // int ASCII = a.codeUnitAt(0);
+  //
+  // if(65 <= ASCII && ASCII <= 90){
+  //   print('uppercase');
+  // }else if(97 <= ASCII && ASCII <= 122){
+  //   print('lowercase');
+  // }else{
+  //   print('nono');
+  // }
+
+  // task 13
+  // int a = 5;
+  // if(a == 1){
+  //   print('dushanba');
+  // }else if(a == 2){
+  //   print('seshanba');
+  // }else if (a == 3){
+  //   print('chorshanba');
+  // }else if (a == 4){
+  //   print('payshanba');
+  // }else if (a == 5){
+  //   print('juma');
+  // }else if (a == 6){
+  //   print('shanba');
+  // }else if (a == 7){
+  //   print('yakshanba');
+  // }else{
+  //   print('HAPSHANBA)))');
+  // }
+
+
+  //task14
+  // int a = 60;
+  // int b = 60;
+  // int c = 60;
+  // bool result = a + b + c == 180;
+  // if(result){
+  //   print(true);
+  // }else{
+  //   print(false);
+  // }
+
+  // task15
+  // int a = 20;
+  // int b = 25;
+  // int c = 30;
+  // if (a + b > c && a + c > b && b + c > a) {
+  //   print(true);
+  // } else {
+  //   print(false);
+  // }
+
+
+  // task16
+  // int a = 23;
+  // int b = 10;
+  // int c = 15;
+  // if(a == b && b == c){
+  //   print("equilateral");
+  // }else if(a == b || b == c || c == a){
+  //   print('isosceles');
+  // }else{
+  //   print('scalene');
+  // }
+
+  // task17
+  // int a = 23;
+  // int b = 10;
+  // int c = 15;
+  // int musbat = 0;
+  // if(a > 0){
+  //   musbat++;
+  // }if (b > 0){
+  //   musbat++;
+  // }if (c > 0){
+  //   musbat++;
+  // }
+  // print(musbat);
+
+  // task 18
+  //  int a = 222;
+  // int b = 12;
+  // if(a < b){
+  //   print("kichik son: $a");
+  // }else if(b < a){
+  //   print("kichik son: $b");
+  // }else if(a == b){
+  //   print("teng");
+  // }
+
+  // task 19
+  // int a = 3;
+  // int b = 10;
+  // int c = 8;
+  // int res = a + b + c;
+  // int ortaQiymat = res ~/ 3;
+  // print(ortaQiymat);
+
+  // task 20
+  // int a = 3;
+  // int b = 5;
+  // int c = 3;
+  // if(a < b && c > b){
+  //   print('1');
+  // }else
+  // if(b>a && b > c){
+  //   print(b);
+  // }else
+  // if(a == b && b == c){
+  //   print('5');
+  // }else{
+  //   print("0");
+  // }
+
+  // task 21
+  // int a = 3;
+  // int b = 10;
+  // int c =  8;
+  // if(a == b){
+  //   print(c);
+  // }else if(c == b){
+  //   print(a);
+  // }else if(c == a){
+  //   print(b);
+  // }else{
+  //   print('0');
+  // }
+
+
+  // task 22
+  // int a = -2;
+  // int b = 6;
+  // int res = 0;
+  // if(a > b){
+  //   res = a - b;
+  // }else if(b > a){
+  //   res = b - a;
+  // }
+  // print("$a va $b ning orasidagi masofa: $res");
+
+  // task 23
+  // int a = 12;
+  // if(a < 0){
+  //   print(a * -1);
+  // }else{
+  //   print(a);
+  // }
+
+  // task 24
+  // int a = 9;
+  // if(0 < a && a <= 9){
+  //   print("$a - bir xonalik son");
+  // }else if (10 <= a && a <= 99){
+  //   print("$a - ikki xonalik son");
+  // }else if(100 <= a && a <= 999){
+  //   print("$a - uch xonalik son");
+  // }else{
+  //   print("son 1 - 999 oraliqda emas!");
+  // }
+
+  // task 25
+  // int a = -12;
+  // int b = -12;
+  // int c = -12;
+  // int d = -12;
+  // if(a > 0 && b > 0 && c > 0 && d > 0){
+  //   print("true");
+  // }else print('false');
+
+  // task 26
+  // int a = 11;
+  // int b = 11;
+  // int c = 1;
+  // if(a.isOdd && b.isOdd && c.isOdd){
+  //   print('1');
+  // }else if(a.isEven && b.isEven && c.isEven){
+  //   print('2');
+  // }else if(a.isOdd || b.isOdd || c.isOdd){
+  //   print('3');
+  // }else print('0');
+
+  // task 27
+  // int a = 12;
+  // int b = 12;
+  // if(a.isEven && b.isEven){
+  //   print('1');
+  // }else if (a.isOdd && b.isOdd){
+  //   print('2');
+  // }else{
+  //   print('0');
+  // }
+
+  // task 28
+  // int a = 12;
+  // int b = 12;
+  // if(a.isEven && b.isEven){
+  //   print('1');
+  // }else if (a.isOdd && b.isOdd){
+  //   print('2');
+  // }else{
+  //   print('0');
+  // }
+
+  // task 29
+  int a = 1;
+  int b = 0;
+  int c = 1;
+  if(a <= 0 || b <= 0 || c <= 0){
+    print('0');
+  }else{
+    print("$a$b$c");
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
